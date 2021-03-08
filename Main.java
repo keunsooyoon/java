136ecd3be9523b6446e7cc7c3712cabceb48d732
Austin <austiny@snu.ac.kr>
2021-03-08 12:25:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+		int sum = 0; 
+		
+		for(int i=1; i<=100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~100 합 : " + sum);
+		}
+	}
+
+
